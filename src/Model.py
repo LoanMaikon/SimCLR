[FIX] loss calculation. Now we can run larger batch sizes

src/Model.py
@@ -186,10 +186,10 @@ class Model():
         self.optimizer = optim.SGD(self.model.parameters(), lr=0.1 * self.linear_evaluation_batch_size / 256, momentum=0.9, weight_decay=0.0)
 
     '''
-    The SimCLR defines lr = 0.3 * batch_size / 256. But in section B.1., it says that for small batch sizes like ours, the bert lr is 0.075 * sqrt(batch_size)
+    The SimCLR defines lr = 0.3 * batch_size / 256 (B.6.)
     '''
     def _load_train_encoder_optimizer(self):
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.075 * np.sqrt(self.train_encoder_batch_size), momentum=0.9, weight_decay=0.0)
+        self.optimizer = optim.SGD(self.model.parameters(), lr=0.3 * self.train_encoder_batch_size / 256, momentum=0.9, weight_decay=0.0)
 
         # Still have to make LARS
 

src/nt_xent.py
@@ -2,34 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# https://github.com/Lightning-Universe/lightning-bolts/blob/master/src/pl_bolts/losses/self_supervised_learning.py
 class nt_xent(nn.Module):
     def __init__(self, temperature):
         super(nt_xent, self).__init__()
         self.temperature = temperature
 
     def forward(self, z1, z2):
-        # Normalizing so cosine similarity works well
-        z1 = nn.functional.normalize(z1, dim=1)
-        z2 = nn.functional.normalize(z2, dim=1)
+        z1 = F.normalize(z1, dim=1)
+        z2 = F.normalize(z2, dim=1)
 
-        batch_size = z1.shape[0]
-        z = torch.cat([z1, z2], dim=0)
+        out = torch.cat([z1, z2], dim=0)
+        n_samples = len(out)
 
-        # z[None, :, :] has shape (1, 2N, d)
-        # z[:, None, :] has shape (2N, 1, d)
-        # similarity_matrix has shape (2N, 2N)
-        similarity_matrix = F.cosine_similarity(z[None, :, :], z[:, None, :], dim=-1)
+        cov = torch.mm(out, out.t().contiguous())
+        sim = torch.exp(cov / self.temperature)
 
-        # Removing diagonal elements since they are the same
-        eye = torch.eye(2 * batch_size, device=z.device).bool()
-        y = similarity_matrix.clone()
-        y[eye] = float('-inf')
+        # Negative similarity
+        mask = ~torch.eye(n_samples, device=sim.device).bool()
+        neg = sim.masked_select(mask).view(n_samples, -1).sum(dim=-1)
 
-        # [0, 1, 2, 3] -> [2, 3, 0, 1]
-        targets = torch.arange(2 * batch_size, device=z.device)
-        targets = (targets + batch_size) % (2 * batch_size)
+        # Positive similarity
+        pos = torch.exp(torch.sum(z1 * z2, dim=-1) / self.temperature)
+        pos = torch.cat([pos, pos], dim=0)
 
-        # Temperature
-        logits = y / self.temperature
-
-        return F.cross_entropy(logits, targets, reduction='mean')
+        return -torch.log(pos / neg).mean()